4085b4c87214f67ebee8043365fca413b1b640bd
梁健辉 <ljk15916807596@qq.com>
2021-12-14 19:38:42 +0800

    fix: 修复问题

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,3 +1,4 @@
+import { history } from 'umi';
 import { extend } from 'umi-request';
 import { message } from 'antd';
 
@@ -17,7 +18,7 @@ const request = extend({
     // console.log(Object.keys(error).map((it: any) => error[it]));
     // 权限无验证 跳转 统一认证页面
     if (error?.response.status === 401) {
-      window.location.href = '/bgs/login';
+      window.location.href = `/bgs/login`;
     }
     if (error?.type === 'Timeout') {
       message.warning('请求超时');
@@ -25,6 +26,18 @@ const request = extend({
     return error;
     /* 异常处理 */
   },
+});
+
+// 赋值头部
+request.interceptors.request.use((url: any, options: any) => {
+  const headers = {
+    Accept: 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
+  };
+  return {
+    url,
+    options: { ...options, headers },
+  };
 });
 
 export { request };
